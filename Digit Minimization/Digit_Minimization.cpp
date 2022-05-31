b2c29da3b145f59e62e6986d79d6f02d5d270b89
kareemsakkary <kareemsakkary101@gmail.com>
2022-05-31 23:42:20 +0200

    daily problem solved

--- Digit Minimization/Digit_Minimization.cpp
+++ Digit Minimization/Digit_Minimization.cpp
@@ -14,13 +14,10 @@ int main()
         string x;
         cin >> x;
         int digits[x.length()];
-        for(int i = 0;i<x.length();i++){
-            digits[i]=int(x[i])-'0';
-        }
         if(x.length()==2){
             cout << digits[1]<< endl;   
         }else{
-            cout << *min_element(digits,digits+x.length()) << endl;
+            cout << *min_element(x.begin(),x.end()) << endl;
         }      
        
         }
