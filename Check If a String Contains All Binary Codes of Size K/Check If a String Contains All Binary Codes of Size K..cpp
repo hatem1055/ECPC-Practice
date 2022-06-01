663fef1139821d1cc80c6b67d2140b3daffefaf9
OmarGaafar1 <92587188+OmarGaafar1@users.noreply.github.com>
2022-06-01 13:18:44 +0200

    Added Problem

--- Check If a String Contains All Binary Codes of Size K/Check If a String Contains All Binary Codes of Size K..cpp
+++ Check If a String Contains All Binary Codes of Size K/Check If a String Contains All Binary Codes of Size K..cpp
@@ -3,32 +3,24 @@ public:
 	bool hasAllCodes(string s, int k) {
 
 		set<string> nums;
-
 		for (int i = 0; i <= s.length(); i++)
 		{
 			string x = s.substr(i, k);
 			if (x.length() != k)
 			{
-
 				continue;
 			}
-			// exists.find(a[i-1] - i) == exists.end()
 			if (nums.find(x) != nums.end())
 			{
-
 				continue;
 			}
 			nums.insert(x);
 			if (nums.size() == pow(2, k))
 			{
 				return true;
-
 			}
-
 		}
 		return false;
-
 	}
-
 };
 
