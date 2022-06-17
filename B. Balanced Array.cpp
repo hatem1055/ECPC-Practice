212d203a4a59d07a2a3e4d08532ccf33642fe4a6
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2022-06-17 12:20:13 +0200

    Create B. Balanced Array.cpp

---
+++ B. Balanced Array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long c, len ;
+    cin >> c ;
+    while(c--){
+        long long sum1=0 , sum2 =0;
+        cin >> len ;
+        if (len % 4 != 0) {
+            cout << "NO" << endl ; 
+        }
+        else{
+            cout << "YES"<< endl ;
+            for(int i = 2 ; i <= len ; i +=2 ){
+                cout << i << " " ;
+                sum1+= i;
+            }
+            for (int i = 1 ; i <= len -2 ; i+=2){
+                cout << i << " ";
+                sum2 += i;
+            }
+            cout << sum1 - sum2 <<endl ;
+        }
+    
+    }
+}
