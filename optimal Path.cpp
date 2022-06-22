90cba00edefb415d9c7c75945f5d232fdc2746f3
hatem1055 <hatemmostafa31@gmail.com>
2022-06-23 00:27:09 +0200

    .

--- optimal Path.cpp
+++ optimal Path.cpp
@@ -1,26 +1,22 @@
+//https://codeforces.com/contest/1700/problem/A
 #include <bits/stdc++.h>
-
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    int t; cin>>t;
-    while(t--){
-        long long int sum=0;
-        int n,m;
-        cin>>n>>m;
-        for(int j=1;j<=m;j++)
-        {
-            sum+=j;
-        }
-        for(int j=2;j<=n;j++)
-        {
-            sum+=m*j;
-        }
-        cout<< sum<<'\n';
-    }
-    return 0;
+using namespace std ;
+int main(){
+        int n ;
+        cin >> n ;
+        while(n--){
+        long long r , c, ans =0, a;
+        cin >> r >> c ;
+            a = r*c ;
+                for(int i = 1 ; i <= c ; i++  ){
+                        ans += i ;
+                }
+                long long  t = c;
+                c += t ;
+                while(c <= a){
+                        ans += c ;
+                        c += t ;
+                }
+                cout << ans << endl ; 
+       }
 }
