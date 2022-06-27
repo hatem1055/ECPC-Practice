4506df6541402e69cc8e5a15c969dfcea2951f23
HodaTouny <100847847+HodaTouny@users.noreply.github.com>
2022-06-28 00:12:09 +0200

    watermelon problem

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int W;
+    cin>>W;
+
+        if(W % 2 != 0 || W<=2){
+            cout<<"NO";
+        }else{
+            cout<<"YES";
+        }
+    return 0;
+}
