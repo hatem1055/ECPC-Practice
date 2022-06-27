f726b6b56f595523bcac4c79e17172922cdf01a9
Nada Mohammed <96924895+nadaamohhamed@users.noreply.github.com>
2022-06-28 00:31:57 +0200

    daily problem solved

---
+++ dailyproblem.cpp
@@ -0,0 +1,12 @@
+// code link: https://codeforces.com/problemset/gymProblem/101883/A
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Please enter the 2 numbers separated by spaces:";
+    cin >> num1 >> num2;
+    cout << num1+num2;
+}
