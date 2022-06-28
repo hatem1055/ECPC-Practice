c56e9d4449f7324fca8eebdce94add2601e03f76
HodaTouny <100847847+HodaTouny@users.noreply.github.com>
2022-06-28 02:07:40 +0200

    Update A boy or A girl .cpp

--- A boy or A girl .cpp
+++ A boy or A girl .cpp
@@ -1,34 +1,21 @@
-#include <iostream>
-using namespace std;
-int main() {
-    string user;
-    int count;
-    cin>>user;
-    bool x = false;
-    if(user.size()>100){
-        x = true;
-    }else{
-        for(int i =0;i<user.size();i++){
-            if(islower(user[i])){
-            }else{
-                cout<<"wrong input";
-                x = true;
-                break;
-            }}}
-    while(x){
-        cin>>user;}
-    count = user.size();
-    for(int i = 0;i<user.size();i++){
-        for(int j = i+1;j<user.size();j++){
-            if(user[i]==user[j]){
+using namespace std ;
+int main(){
+    string str;
+    cin >> str ;
+    int count = str.size();
+    for(int i = 0 ; i < str.size()-1; i++){
+        for(int j = i + 1 ; j < str.size() ; j++){
+            if (str[i] == str[j]){
                 count--;
                 break;
-            }}}
-    cout<<count<<endl;
-    if(count%2==0){
-        cout<<"CHAT WITH HER!";
-    }else{
-        cout<<"IGNORE HIM!";
+            }
+        }
     }
-    return 0;
+    cout << count << endl ;
+    if(count % 2 == 0){
+        cout << "CHAT WITH HER!";
+    }
+    else{
+        cout << "IGNORE HIM!";
+    }
 }
