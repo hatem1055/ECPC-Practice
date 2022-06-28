7181853eab066f80b1a2056db1c487beb6810990
fares osama <101796917+faresosama12@users.noreply.github.com>
2022-06-28 18:49:25 +0200

    problem solved :)

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//link cade:
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,sum;
+    cin >> a>>b>>c>>d;
+    sum=(a*b)-(c*d);
+    cout<<"Difference = "<<sum;
+
+    return 0;
+   }
