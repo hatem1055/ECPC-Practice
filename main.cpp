8209c32bd26d6ca6c6ad76327a9f2518783276da
Abdelrahman-Tolba <102567924+Abdelrahman-Tolba@users.noreply.github.com>
2022-06-28 23:58:25 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,32 @@
-//link cade:
-// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
-
 #include <iostream>
+#include <string>
+
+int i;
 using namespace std;
 
 int main() {
-    long long a,b,c,d,sum;
-    cin >> a>>b>>c>>d;
-    sum=(a*b)-(c*d);
-    cout<<"Difference = "<<sum;
+    int size;
+    cout<<"Enter the number of tickets."<<endl;
+    cin >> size;
+    string arr[size];
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+        while (arr[i].size() != 6) {
+            cout << "The ticket must be 5 digits" << endl;
+            cin >> arr[i];
 
-    return 0;
-   }
+        }
+
+    }
+    for(int j=0;j< size ;j++){
+        if (((arr[j].at(1)-0) + (arr[j].at(0)-0) + (arr[j].at(2)-0)) == (arr[j].at(3)-0) + (arr[j].at(4)-0) + (arr[j].at(5)-0)) {
+            cout << "YES"<<endl;
+
+        }else cout<<"NO"<<endl;
+
+
+
+
+    }
+}
+
