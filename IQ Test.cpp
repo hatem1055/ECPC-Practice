8019b70e15a5becf609fab52c4662d4580f78ef3
Tahawyy <101939990+Tahawyy@users.noreply.github.com>
2022-06-28 16:10:48 +0200

    daily

--- IQ Test.cpp
+++ IQ Test.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
 using namespace std;
-int main(){
-  int size,count=0;
-  cin >> size;
-  int arr[size];
-  for(int i=0;i<size;i++){
-      cin >> arr[i];
-  }
-  for(int i=0;i<size;i++){
-      if(arr[i]%2==0)
-          count++;
 
-  }
-  if(count == size-1){
-      for(int i=0;i<size;i++){
-          if((arr[i]%2)!=0){
-              cout << i+1;
-              break;}
-      }
-  }
-  else{
-      for(int i=0;i<size;i++){
-          if((arr[i]%2)==0){
-              cout << i+1;
-              break;
-      }}
-
-  }
-
-
+int main() {
+    int n , even =0,odd = 0;
+    cin >> n;
+    int arr[n];
+    for (int i =0;i < n;i++){
+        cin >> arr[i];
+        if(arr[i] % 2 == 0){
+            even++;
+        }
+        else{
+            odd++;
+        }
+    }
+    if(even > odd){
+        for(int i =0; i < n;i++){
+            if(arr[i] %2 !=0){
+                cout << i+1;
+                break;
+            }
+        }
+    }
+    else{
+        for (int i =0;i < n;i++){
+            if(arr[i] %2 ==0){
+                cout << i+1;
+                break;
+            }
+        }
+    }
 
 }
-
-
-
-
-
 
