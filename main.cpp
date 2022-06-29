06bb4da138e00820f97ad3b028394ce77ca48ffc
fares osama <101796917+faresosama12@users.noreply.github.com>
2022-06-30 00:16:43 +0200

    problem solved :)

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
+//code link:https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
 #include <iostream>
+#include <iomanip>
 using namespace std;
-int main()
-{
-int k,n,w;
-cin >> k >> n >> w;
-int sum=0;
-for(int i = 1; i <=w; i++){
-    sum+=i;
-}
-int d = (sum*k)-n;
-if(d<0){
-    cout << "0";
-}
-else{
-    cout << d;
-}
+int main(){
+    cout << fixed << setprecision(9);
+   double x,area;
+    cin>>x;
+
+    area= (x*x) * 3.141592653;
+    cout<<area;
+    return 0;
 }
