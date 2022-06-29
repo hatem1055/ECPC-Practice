b440031d10c04f7d6d4f47c617041817a3f18a02
Ahmedyehia12 <97615630+Ahmedyehia12@users.noreply.github.com>
2022-06-29 22:15:44 +0200

    Add files via upload
    https://codeforces.com/problemset/problem/486/A

---
+++ Code Killers/Calculating Function.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+        long long n;
+        cin>>n;
+        if(n%2==0)
+        {
+            cout<<n/2<<endl;
+        }
+        else
+        {
+            cout<<-(n+1)/2;
+        }
+
+    }
+
