932370aa25a4b3d2d6b10fec746e18df9ee7325e
shahd abdelmoamen <102470488+shahdmoamen@users.noreply.github.com>
2022-06-29 23:33:53 +0200

    Add files via upload

---
+++ SYM-Error/Fitness.cpp
@@ -0,0 +1,14 @@
+// created by shahd moamen
+// link :https://www.codechef.com/submit/FIT
+#include <iostream>
+using namespace std;
+
+int main() {
+int x,t;
+cin >>t;
+ for(int i = 0; i< t; i++) {
+     cin >> x;
+        cout <<x*2*5<<endl;
+    }
+	return 0;
+}
