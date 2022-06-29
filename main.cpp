beef572569cb9bf121501886b70c7da3dee26cee
shahd abdelmoamen <102470488+shahdmoamen@users.noreply.github.com>
2022-06-29 23:24:07 +0200

    fitness

--- main.cpp
+++ main.cpp
@@ -1,30 +1,14 @@
+// created by shahd moamen
+// link :https://www.codechef.com/submit/FIT
 #include <iostream>
-#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-string name;
-cin >> name;
-int counter;
-int flag = 1;
 
-for (int i = 0; i < name.size(); i++){
-    for(int z=i+1; z < name.size(); z++){
-        if(name[i] == name[z]){
-            flag = 0;
-            break;
-        }
+int main() {
+int x,t;
+cin >>t;
+ for(int i = 0; i< t; i++) {
+     cin >> x;
+        cout <<x*2*5<<endl;
     }
-    if(flag){
-        counter++;
-    }
-    flag = 1;
+	return 0;
 }
-    if(counter%2 == 0){
-        cout << "CHAT WITH HER!";
-    }
-    else{
-        cout << "IGNORE HIM!";
-    }
-}
-
