41160c13fd7e28e8f13156d6598507046b18c2b8
nourhan031 <92525531+nourhan031@users.noreply.github.com>
2022-06-30 14:30:58 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
-//code link:https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
 #include <iostream>
-#include <iomanip>
+#include<string>
 using namespace std;
-int main(){
-    cout << fixed << setprecision(9);
-   double x,area;
-    cin>>x;
-
-    area= (x*x) * 3.141592653;
-    cout<<area;
-    return 0;
+int main()
+{
+string s;
+cin >> s;
+int count = 0;
+for(int i = 0; i < s.size(); i++){
+    if(s[i]=='4' || s[i]=='7'){
+        count++;
+    }
+}
+if(count == 4 || count == 7){
+    cout << "YES";
+}
+else{
+    cout << "NO";
+}
 }
