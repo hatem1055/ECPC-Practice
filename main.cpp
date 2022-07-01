d19e8f6dc52c820b1feddfe156a12d48bd203f9e
Abdelrahman-Tolba <102567924+Abdelrahman-Tolba@users.noreply.github.com>
2022-07-01 20:44:43 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int nbox,turns;
-    cin>>turns;
-    while(turns--) {
-        cin >> nbox;
-        int totaleated_candies=0;
-        int nofcandies[nbox], eatencandies[nbox];
-        for (int i = 0; i < nbox; i++) {
-            cin >> nofcandies[i];
+    int t;
+    cin>>t;
+    while(t--){
+        int nofplayers,appendance{0};
+        nofplayers = 4;
+        int arr[nofplayers];
+        for(int i =0; i <nofplayers;i++){
+            cin>>arr[i];
         }
-        sort(nofcandies, nofcandies + nbox);
-        for (int j = 0; j < nbox; j++) {
-            eatencandies[j] = nofcandies[j] - nofcandies[0];
+        for(int j=0;j<1;j++){
+            for(int c=1;c<nofplayers;c++){
+                if(arr[j]<arr[c]){
+                    appendance+=1;
+                }
+                else{continue;}
+            }
         }
-        for (int c = 0; c < nbox; c++) {
-            totaleated_candies += eatencandies[c];
-        }
-
-        cout << totaleated_candies<<endl;
+        cout<<appendance<<endl;
     }
 }
