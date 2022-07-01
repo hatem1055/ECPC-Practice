4926ef61e518dc66052be46524d7b2f4fc9cac2a
nourhan031 <92525531+nourhan031@users.noreply.github.com>
2022-07-01 14:27:30 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,16 +5,6 @@ int main()
 {
 string s;
 cin >> s;
-int count = 0;
-for(int i = 0; i < s.size(); i++){
-    if(s[i]=='4' || s[i]=='7'){
-        count++;
-    }
-}
-if(count == 4 || count == 7){
-    cout << "YES";
-}
-else{
-    cout << "NO";
-}
+s[0]=toupper(s[0]);
+cout << s;
 }
