38f1c8e2104bd9c77967ebb3e5e0960d6e4846af
nourhan031 <92525531+nourhan031@users.noreply.github.com>
2022-07-02 17:43:42 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        int nofplayers,appendance{0};
-        nofplayers = 4;
-        int arr[nofplayers];
-        for(int i =0; i <nofplayers;i++){
-            cin>>arr[i];
-        }
-        for(int j=0;j<1;j++){
-            for(int c=1;c<nofplayers;c++){
-                if(arr[j]<arr[c]){
-                    appendance+=1;
-                }
-                else{continue;}
-            }
-        }
-        cout<<appendance<<endl;
+int main()
+{
+int m,a,b,n;
+cin >> n >> m >> a >> b;
+
+if(m*a > b){
+    int x = (n%m)*a;
+    if(x>b){
+        cout << n/m*b + b;
     }
+    else{
+        cout << n/m*b + x;
+    }
+}
+else{
+    cout << n*a;
+}
 }
