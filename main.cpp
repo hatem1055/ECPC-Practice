ef07d284a3f8ae90962456876dbb7038f1e3ea91
Abdelrahman-Tolba <102567924+Abdelrahman-Tolba@users.noreply.github.com>
2022-07-04 17:05:07 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    long long m,n,a,x,y;
-    cin >> m >> n >> a;
 
-    if(m%a==0){
-        x = m/a;
+int main() {
+    int num,solved{0},accepted{0};
+    cin>>num;
+    int arr[num][3];
+    for(int c=0;c<num;c++){
+        for(int x=0;x<3;x++){
+            cin>>arr[c][x];
+        }
     }
-    else{
-        x = m/a +1;
+    for(int i = 0;i<num;i++){
+        solved=0;
+        for(int j=0;j < 3;j++){
+           if(arr[i][j]==1){
+               solved+=1;
+           }else{
+               continue;
+           }
+        }
+        if(solved>=2){
+            accepted+=1;
+        }
     }
+    cout<<accepted;
 
-    if(n%a==0){
-        y = n/a;
-    }
-    else{
-        y = n/a +1;
-    }
-    cout << x*y << endl;
-    return 0;
+
 }
