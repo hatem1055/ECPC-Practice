554ff28cf0fe52299f05ed9682d28fa544642ea4
nourhan031 <92525531+nourhan031@users.noreply.github.com>
2022-07-04 19:49:05 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,18 @@
 #include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
+int main()
+{
+int x;
+cin >> x;
+int result = 0;
 
-int main() {
-    int num,solved{0},accepted{0};
-    cin>>num;
-    int arr[num][3];
-    for(int c=0;c<num;c++){
-        for(int x=0;x<3;x++){
-            cin>>arr[c][x];
-        }
+while(x>0){
+    if(x%2==1){
+        result++;
     }
-    for(int i = 0;i<num;i++){
-        solved=0;
-        for(int j=0;j < 3;j++){
-           if(arr[i][j]==1){
-               solved+=1;
-           }else{
-               continue;
-           }
-        }
-        if(solved>=2){
-            accepted+=1;
-        }
-    }
-    cout<<accepted;
+    x/=2;
+}
 
-
+cout << result << endl;
 }
