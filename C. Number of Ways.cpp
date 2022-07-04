32ecbe7bfee60b0b1db41c0a5059fded0422528d
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2022-07-04 22:45:05 +0200

    Daily problem

---
+++ C. Number of Ways.cpp
@@ -0,0 +1,38 @@
+//https://codeforces.com/contest/466/problem/C
+//created by: Rana Essam 
+//last edit:4/7/2022
+
+#include <bits/stdc++.h>
+using namespace std ;
+int main(){
+    long long n , x, sum=0,  suf = 0, ans = 0 , pref=0 ;
+    cin >> n ;
+    long long * nums = new long long [n];
+    long long * dp = new long long [n];
+    for(long long  i = 0 ; i < n ; i++){
+       cin >> nums[i];
+       sum += nums[i];
+    }
+    if( sum % 3 != 0){
+      cout << 0 ;
+      return 0 ;
+    }
+    for(long long i = n-1 ; i >=0 ; i--){
+        suf += nums[i];
+        if (i < n-1)
+            dp[i] = dp[i+1];
+        else
+            dp[i] = 0 ;
+        if (suf == sum/3){
+            dp[i] ++ ;
+        }
+    }
+    for(int i = 0 ; i < n-2; i++){
+        pref += nums[i];
+        if(pref == sum / 3)
+            ans += dp[i+2];
+
+    }
+cout << ans ;
+
+}
