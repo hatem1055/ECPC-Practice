e72cbdcfac866781aeded0d8f56c8c2d52ae7d1f
Abdelrahman-Tolba <102567924+Abdelrahman-Tolba@users.noreply.github.com>
2022-07-06 00:31:50 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
-#include<bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-int price, coin;
-cin >> price >> coin;
-int counter = 0;
-int result = 0;
 
-while(true){
-    counter++;
-    result = price*counter;
-
-    if(result%10 == 0 || result%10 == coin){
-        break;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        vector<int>minis;
+        int row, len;
+        cin>>row;
+        cin>>len;
+        string tot[row];
+        for(int i=0;i<row;i++){
+            cin>>tot[i];
+        }
+        for(int i = 0; i < row; i++){
+            for(int j = i + 1; j < row; j++){
+                int sumForWrd = 0;
+                for(int k = 0; k < len; k++){
+                    sumForWrd+= abs(tot[i][k] - tot[j][k]);
+                }
+                minis.push_back(sumForWrd);
+            }
+        }
+        int min = *min_element(minis.begin(), minis.end());
+        cout << min << endl;
     }
-}
-
-cout << counter << endl;
 }
