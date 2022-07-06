85070fa92f14085eaea54de5951964622f99b0d3
Tahawyy <101939990+Tahawyy@users.noreply.github.com>
2022-07-06 14:34:13 +0200

    daily

--- Code-Killers/Calculating Function.cpp
+++ Code-Killers/Calculating Function.cpp
@@ -1,17 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
 
-        long long n;
-        cin>>n;
-        if(n%2==0)
-        {
-            cout<<n/2<<endl;
-        }
-        else
-        {
-            cout<<-(n+1)/2;
-        }
-
+int main() {
+    long long n,ans;
+    cin >> n;
+    if(n % 2 ==0){
+        ans = n/2;
     }
-
+    else{
+        ans = ((n+1)/2)*(-1);
+    }
+    cout << ans;
+}
