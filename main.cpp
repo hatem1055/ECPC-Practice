f8273d1377b31f8ec2b9ded9a2312c7e2a619c99
nourhan031 <92525531+nourhan031@users.noreply.github.com>
2022-07-08 13:00:48 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
-#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
 int a;
 cin >> a;
 int arr[a];
-
-for(int i = 0; i<a; i++){
+for(int i = 0; i < a; i++){
     cin >> arr[i];
 }
-
-sort(arr,arr+a);
-for(int i = 0; i<a; i++){
-    cout << arr[i] << " ";
+for(int j = 1; j <= a; j++){
+    for(int i = 0; i < a; i++){
+        if(arr[i] == j){
+            cout << i+1 << " ";
+        }
+    }
 }
-
-return 0;
 }
