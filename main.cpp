7955f4ae5081b853ae73322d972ec81ef7db4f32
Abdelrahman-Tolba <102567924+Abdelrahman-Tolba@users.noreply.github.com>
2022-07-10 21:37:01 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
-#include <cmath>
+#include <string>
+#include <cstring>
+#include <cctype>
 using namespace std;
-int main()
-{
-int i,j,x=0;
-for(i=1; i<=5;i++){
-    for(j=1;j<=5; j++){
-        cin>>x;
-        if(x==1){
-            cout<< abs(i-3)+abs(j-3)<<endl;
+
+int main() {
+    int low{0},up{0};
+    char ch;
+    string tot;
+    cin>>tot;
+    for(int i=0;i<tot.size();i++){
+        if(isupper(tot[i])){
+            up+=1;
+        }else if(islower(tot[i])){
+            low+=1;
         }
     }
-}
+   if(low > up){
+        for(int j=0;j<tot.size();j++){
+            ch= tolower(tot[j]);
+            cout<<ch;
+        }
+   }else if(up>low){
+        for(int x=0;x<tot.size();x++){
+            ch= toupper(tot[x]);
+            cout<<ch;
+        }
+   }else{
+       for(int j=0;j<tot.size();j++){
+               ch= tolower(tot[j]);
+               cout<<ch;
+       }
+
+   }
+
+
 }
