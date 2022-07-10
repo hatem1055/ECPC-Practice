d9130778c6e268a707450cbed11525164e4a376d
shehabsayed <106034258+shehabsayed@users.noreply.github.com>
2022-07-11 00:57:39 +0200

    Daily problem solved-Keyboard

--- Others/main.cpp
+++ Others/main.cpp
@@ -1,26 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int c=0;
-int greedy(int n,int arr[],int sum,int comp){
-    if(comp <= sum) {
-        c++;
-        return greedy(n-1,arr,sum-arr[n-1],comp+arr[n-1]);
-        }
-    else return c;
-}
 int main() {
     std::ios_base::sync_with_stdio(0);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    int n,sum=0,comp=0;
-    cin >> n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
-        sum += arr[i];
-    }
-    sort(arr,arr+n);
-    if(n == 1) cout << "1";
-    else cout << greedy(n,arr,sum,comp);
+    char c;
+    cin >> c;
+    string s;
+    cin >> s;
+    string st = "qwertyuiopasdfghjkl;zxcvbnm,./";
+    for (int i = 0; i < s.size(); i++) {
+            int m = st.find(s[i]);
+            if(c == 'R') cout << st[m-1];
+            else cout << st[m+1];
+        }
     return 0;
 }
