d054b09d1a7b07d4c4b33e0c6582cabe62542027
nourhan031 <92525531+nourhan031@users.noreply.github.com>
2022-07-11 16:25:33 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,38 +1,16 @@
 #include <iostream>
-#include <string>
-#include <cstring>
-#include <cctype>
 using namespace std;
-
-int main() {
-    int low{0},up{0};
-    char ch;
-    string tot;
-    cin>>tot;
-    for(int i=0;i<tot.size();i++){
-        if(isupper(tot[i])){
-            up+=1;
-        }else if(islower(tot[i])){
-            low+=1;
-        }
+int main()
+{
+int a,n;
+cin >> a >> n;
+for(int i=0; i<n; i++){
+    if(a%10==0){
+        a/=10;
     }
-   if(low > up){
-        for(int j=0;j<tot.size();j++){
-            ch= tolower(tot[j]);
-            cout<<ch;
-        }
-   }else if(up>low){
-        for(int x=0;x<tot.size();x++){
-            ch= toupper(tot[x]);
-            cout<<ch;
-        }
-   }else{
-       for(int j=0;j<tot.size();j++){
-               ch= tolower(tot[j]);
-               cout<<ch;
-       }
-
-   }
-
-
+    else{
+        a--;
+    }
+}
+cout << a;
 }
