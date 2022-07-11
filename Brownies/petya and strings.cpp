9a86bef702e67f6e8227ec8d18d1d2d99fe857e6
mayahesham <96448594+mayahesham@users.noreply.github.com>
2022-07-11 23:47:50 +0200

    daily problem solved

--- Brownies/petya and strings.cpp
+++ Brownies/petya and strings.cpp
@@ -1,25 +1,30 @@
-//https://codeforces.com/problemset/problem/112/A
+#include <cctype>
 #include <iostream>
+#include <algorithm>
 using namespace std;
-int main() {
-    string x,y;
-    int a=0,b=0;
-    cin>>x>>y;
-    for(int i =0;i<x.size();i++){
-    x[i] = tolower(x[i]);
-    a+=x[i];
-    y[i] = tolower(y[i]);
-    b+=y[i];
-    if(x[i] == y[i]) {
-    }else{
-        if(x[i] < y[i]){
-            cout<<"-1";
-            return 0;
-        }else if(x[i] >y[i]){
-        cout<<"1";
-            return 0;
-    }
-    }}
-    cout<<"0";
+
+int main()
+{
+    int n;
+
+    string str1,str2;
+    cin>>str1>>str2;
+
+    for_each(str1.begin(), str1.end(), [](char & c) {
+        c = ::tolower(c);
+    });
+    for_each(str2.begin(), str2.end(), [](char & d) {
+        d = ::tolower(d);
+    });
+
+
+
+    if(str1>str2)
+        cout<<1;
+    else if (str2>str1)
+        cout<<-1;
+    else if (str1==str2)
+        cout<<0;
+
     return 0;
 }
