d0d03cbe03b99854618043cd62a27d723835bbd4
shehabsayed <106034258+shehabsayed@users.noreply.github.com>
2022-07-12 19:19:23 +0200

    Daily problem solved-Cypher

--- Others/main.cpp
+++ Others/main.cpp
@@ -4,15 +4,33 @@ int main() {
     std::ios_base::sync_with_stdio(0);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    char c;
-    cin >> c;
-    string s;
-    cin >> s;
-    string st = "qwertyuiopasdfghjkl;zxcvbnm,./";
-    for (int i = 0; i < s.size(); i++) {
-            int m = st.find(s[i]);
-            if(c == 'R') cout << st[m-1];
-            else cout << st[m+1];
+    long long t;
+    cin >> t;
+    for(long long i=0;i<t;i++){
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int j=0;j<n;j++){
+            cin >> arr[j];
         }
+        for(int j=0;j<n;j++){
+            int m;
+            cin >> m;
+            string s;
+            cin >> s;
+            for(int k=0;k<s.size();k++){
+                if(s[k] == 'U' && arr[j] == 0) arr[j] = 9;
+                else if(s[k] == 'D' && arr[j] == 9) arr[j] = 0;
+                else{
+                    if(s[k] == 'U') arr[j]--;
+                    else arr[j]++;
+                }
+            }
+        }
+        for(int j=0;j<n;j++){
+            cout << arr[j] << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
