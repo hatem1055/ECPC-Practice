e44b7e41df2a1be33517c1482efd6439fc21ae47
Abdelrahman-Tolba <102567924+Abdelrahman-Tolba@users.noreply.github.com>
2022-07-12 23:14:38 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <cctype>
+
 using namespace std;
 
-int main() {
-    int n,h,ans{0};
-    cin>>n;
-    cin>>h;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    for(int j=0;j<n;j++){
-        if(arr[j]>h){
-            ans+=2;
-        }else{
-            ans+=1;
-        }
-    }
-    cout<<ans;
-
+int main()
+{
+    string s;
+    cin >> s;
+    s[0] = towupper(s[0]);
+    cout << s << endl;
+    return 0;
 }
