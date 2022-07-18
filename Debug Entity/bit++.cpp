a078c3dc248cc7830b23b70ee1c7be0066f2e641
Noor Eyad <93126253+nooreyad@users.noreply.github.com>
2022-07-18 17:38:04 +0200

    Add files via upload

---
+++ Debug Entity/bit++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, x = 0;
+    string operation;
+    cin >> n;
+    if (n < 1 || n > 150){
+        cin >> n;
+    }
+    for (int i = 0; i < n; i++){
+        cin >> operation;
+        if (operation == "++X" || operation == "X++"){
+            x++;
+        }
+        else if (operation == "--X" || operation == "X--"){
+            x--;
+        }
+    }
+    cout << x;
+}
