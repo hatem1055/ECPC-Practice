ba6d86698c1f9320cfaa4a99ec1828e75b86fe08
DarkenSoda <mahmoudadel200190@yahoo.com>
2022-07-23 17:56:33 +0200

    a

--- Debug Entity/next round.cpp
+++ Debug Entity/next round.cpp
@@ -1,28 +1,17 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
-int main(){
-    int n, k, c, score, participant = 0;
-    vector<int> vec;
+int main() {
+    int n , k, count = 0;
     cin >> n >> k;
-    if (n < 1 || k > n || n > 50){
-        cin >> n >> k;
+    int array[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> array[i];
     }
-    for (int i = 0; i < n; i++){
-        cin >> score;
-        vec.push_back(score);
-    }
-    for (int i = 0; i < vec.size(); i++){
-        if (i + 1 == k){
-            c = vec[i];
+    for (int i = 0; i < n; ++i) {
+        if(array[i] >= array[k-1] && array[i] > 0){
+            count++;
         }
     }
-    for (int m : vec){
-        if (m >= c && m != 0){
-            participant++;
-        }
-    }
-    cout << participant;
+    cout << count;
 }
