a22d9f03353cdb177cd5b7c463eef4f532d5c438
Abdelrahman-Tolba <102567924+Abdelrahman-Tolba@users.noreply.github.com>
2022-07-24 20:18:33 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
-#include<vector>
-#include<bits/stdc++.h>
+#include<string>
 using namespace std;
-int main()
-{
-    vector<int>vector1;
-    vector<int>vector2;
-    vector1.push_back(10);
-    vector1.push_back(20);
-    cout<<vector1.at(0)<<endl;
-    cout<<vector1.at(1)<<endl;
-    vector2.push_back(100);
-    vector2.push_back(200);
-    vector<vector<int>>vector_2d;
-    vector_2d.push_back(vector1);
-    cout << vector_2d[0][0]<<endl;
-    cout << vector_2d[0][1]<<endl;
-    vector_2d.push_back(vector2);
-    cout<<vector_2d[1][0]<<endl;
-    cout<<vector_2d[1][1]<<endl;
-    vector_2d.at(0).at(0)=1000;
-    cout << vector_2d.at(0).at(0)<<endl;
-    cout << vector_2d.at(0).at(1)<<endl;
 
 
+int main() {
+    int n, x(0);
+    cin >> n;
 
+    string s;
+    while (n--) {
+        cin >> s;
+        if (s[1] == '+') {
+            ++x;
+        } else {
+            --x;
+        }
+    }
+
+    cout << x << endl;
+    return 0;
 }
