ceecdf423bc4c3e141b71cfbaf578c6d776211cd
HodaTouny <100847847+HodaTouny@users.noreply.github.com>
2022-07-26 00:10:11 +0200

    Add files via upload

--- Brownies/main.cpp
+++ Brownies/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    string x,x1,x2;
-    cin>>x;
-   for(int i=0;i<3;i++){
-       x1[i] = x[(x.size()-1)-i];
-   }
-    return 0;
+	 int n,k;
+	 cin>>n>>k;
+	 cout<<(n*k)/2;
+	return 0;
 }
