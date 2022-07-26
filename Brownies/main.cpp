283ef9aa25c1f7bdb9476b2f3e8a5c8cc4afa24e
HodaTouny <100847847+HodaTouny@users.noreply.github.com>
2022-07-26 23:56:59 +0200

    Chat room

--- Brownies/main.cpp
+++ Brownies/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-	 int n,k;
-	 cin>>n>>k;
-	 cout<<(n*k)/2;
-	return 0;
+    string s,l="hello";
+    int c=0;
+    cin>>s;
+    int b =0;
+    for(int k =0;k<s.size();k++){
+        if(s[k] == l[b]){
+            b++;
+            c++;
+        }
+        if(c == 5){
+            cout<<"YES";
+            return 0;
+        }
+    }
+    cout<<"NO";
+
+    return 0;
 }
