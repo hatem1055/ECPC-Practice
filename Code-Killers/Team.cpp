069b8c97188405bca4ec45c31317bb075361a15b
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2022-07-31 12:49:41 +0200

    Add files via upload
    https://codeforces.com/problemset/problem/231/A

--- Code-Killers/Team.cpp
+++ Code-Killers/Team.cpp
@@ -1,14 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include<algorithm>
+#define ll long long
 using namespace std;
-
-int main() {
-    int n,p,v,t,c=0;
-    cin >> n;
-    while(n--){
-     cin >> p >> v >> t;
-     if(p+v+t>=2){
-         c++;
-     }
-    }
-    cout << c;
+void files() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
 }
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n,count,c=0;
+    cin>>n;
+    while(n--){
+        count=0;
+       int arr[3];
+        for(int i=0;i<3;i++){
+            cin>>arr[i];
+            if(arr[i]==1)
+                count++;
+        }
+        if(count>=2)
+            c++;
+
+
+    }
+    cout<<c;
+
+}
+
+
+
+
+
+
