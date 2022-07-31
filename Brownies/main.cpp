c501bff89fc6743be9c3c01190725a23530fc3fa
HodaTouny <100847847+HodaTouny@users.noreply.github.com>
2022-07-31 23:15:41 +0200

    summation from 1 to N

--- Brownies/main.cpp
+++ Brownies/main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    string s,l="hello";
-    int c=0;
-    cin>>s;
-    int b =0;
-    for(int k =0;k<s.size();k++){
-        if(s[k] == l[b]){
-            b++;
-            c++;
-        }
-        if(c == 5){
-            cout<<"YES";
-            return 0;
-        }
-    }
-    cout<<"NO";
-
+   long long n; cin>>n;
+   cout<<(n*(n+1))/2;
     return 0;
 }
