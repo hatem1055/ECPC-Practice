5671ed6a34d98b828e8ecf58d650b0b188e315b1
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2022-08-03 12:49:25 +0200

    Add files via upload
    https://codeforces.com/problemset/problem/50/A

---
+++ Code-Killers/Domino.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+void files() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n,k;
+    cin>>n>>k;
+    if(n==1&&k==1){
+        cout<<0<<endl;
+        return 0;
+    }
+    for(int i=1;i++;){
+        int ref = 2;
+        if((ref*i)<=(n*k)){
+        }
+        else{
+            cout<<i-1<<endl;
+            return 0;
+        }
+
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // alpha[97,122]
+    // x y         a[i]==x    a[i]=y   a[
+
+
+
+
+
+
+
+
+
+
