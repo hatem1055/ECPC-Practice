d9ac502567dc4283ed521b8b13482b53ac3c4951
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2022-08-09 12:58:31 +0200

    Add files via upload
    https://codeforces.com/problemset/problem/281/A

---
+++ Code-Killers/CAP.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include<math.h>
+#define ll long long
+#define MOD 1000000007
+
+using namespace std;
+void files() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string s;
+    cin>>s;
+    s[0]= toupper(s[0]);
+    cout<<s<<endl;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
