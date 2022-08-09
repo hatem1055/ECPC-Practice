7c359b2f9117732206918abcd5f098a9655d739b
Nada Mohammed <96924895+nadaamohhamed@users.noreply.github.com>
2022-08-09 23:37:48 +0200

    Add files via upload

---
+++ Brownies/Kefa and First Steps.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define NadaM ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define endl '\n'
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+typedef long long ll;
+
+void solve() {
+  int n, ans = 0;
+  cin >> n;
+  int arr[n];
+  for(int i = 0;i<n;i++){
+      cin >> arr[i];
+  }
+  vector <int> max;
+    for(int i = 0;i<n;i++){
+        if(arr[i] <= arr[i+1])
+            ans++;
+        else{
+            ans++;
+            max.pb(ans);
+            ans = 0;
+        }
+        if(i == n - 1)
+            max.pb(ans);
+    }
+    cout << *max_element(all(max)) << endl;
+}
+
+int main(){
+    NadaM
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int t = 1;
+//    cin >> t;
+    while(t--)
+        solve();
+}
