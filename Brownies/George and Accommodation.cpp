8fff86cd0a42e175c4c1572975936434d40fbf6b
Nada Mohammed <96924895+nadaamohhamed@users.noreply.github.com>
2022-08-18 20:07:48 +0200

    Add files via upload

--- Brownies/George and Accommodation.cpp
+++ Brownies/George and Accommodation.cpp
@@ -1,14 +1,39 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-    int n,k,l,c=0;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>k>>l;
-        if(k<(l-1)){
-            c++;
-        }
+
+#define NadaM ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define endl '\n'
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+typedef long long ll;
+
+void solve(){
+    int n, p, q, ans = 0;
+    cin >> n;
+    while(n--){
+        cin >> p >> q;
+        if(q - p >= 2)
+            ans++;
     }
-    cout<<c;
-    return 0;
+    cout << ans << endl;
 }
+
+int main(){
+    NadaM
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int t = 1;
+//    cin >> t;
+    while(t--)
+        solve();
+}
+
+
+
+
