0c0b32ebb9ee7a528b898b723caee35c8d1ef388
Nada Mohammed <96924895+nadaamohhamed@users.noreply.github.com>
2022-08-19 01:28:56 +0200

    Add files via upload

--- Brownies/Nearly Lucky Number.cpp
+++ Brownies/Nearly Lucky Number.cpp
@@ -1,20 +1,50 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-   string x;
-   cin>>x;
-   int count=0;
-   for(int i=0;i<x.size();i++){
-       if((x[i] == '4') || (x[i] == '7')){
-           count++;
 
-       }
+#define NadaM ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define endl '\n'
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
 
-   }
-   if(count == 4 || count == 7) {
-       cout << "YES";
-   }else{
-       cout<<"NO";
-   }
-    return 0;
+typedef long long ll;
+
+void solve(){
+    ll n;
+    int cnt = 0;
+    cin >> n;
+    while(n > 0){
+        if(n % 10 == 7 || n % 10 == 4)
+            cnt++;
+        n /= 10;
+    }
+    bool flag = false;
+    while(cnt > 0){
+        if(cnt % 10 == 7 || cnt % 10 == 4){
+            flag = true;
+        }
+        else{
+            flag = false;
+            break;
+        }
+        cnt /= 10;
+    }
+    if(flag)
+        yes
+    else
+        no
+}
+
+int main(){
+    NadaM
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int t = 1;
+//    cin >> t;
+    while(t--)
+        solve();
 }
